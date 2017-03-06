6cc6b838cc52a5c9a1c42660537fa0c44abbc89a
Shaojung Lu <shaojung@gmail.com>
2017-03-06 11:32:35 +0800

    2 button

--- app/src/main/java/com/example/yvtc/yvd030602/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd030602/MainActivity.java
@@ -7,7 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener
+    {
     Button btn, btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,21 +18,20 @@ public class MainActivity extends AppCompatActivity {
 
         btn = (Button) findViewById(R.id.button);
         btn2 = (Button) findViewById(R.id.button2);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("BTN", "Test1");
-
-
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("BTN", "Test2");
-
-
-            }
-        });
+        btn.setOnClickListener(this);
+        btn2.setOnClickListener(this);
     }
-}
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+                case R.id.button:
+                    Log.d("BTN", "Test1");
+                    break;
+                case R.id.button2:
+                    Log.d("BTN", "Test2");
+                    break;
+            }
+        }
+    }
